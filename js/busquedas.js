100b97bdaaa55a26c06d4b7ad5d10da2fd0661ca
marisa <marisaviotti@gmail.com>
2024-06-21 11:20:34 -0300

    barra de busqueda

---
+++ js/busquedas.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('input[type="text"]');
+input.addEventListener('input', function() {
+    // Realiza una búsqueda en tiempo real aquí y muestra los resultados.
+});
