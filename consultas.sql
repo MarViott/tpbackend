c7bb64e3d5acbeb8876e89fcb86f078fb41f317a
marisa <marisaviotti@gmail.com>
2024-06-24 19:16:37 -0300

    primera consulta base de datos

--- consultas.sql
+++ consultas.sql
@@ -8,9 +8,11 @@ SELECT Nombre, Apellido FROM usuarios WHERE Id = 5 OR Id = 6;
 
 SELECT Nombre, Apellido FROM usuarios WHERE Id = 5 OR (Id = 6 AND Ocupacion = "estudiante");
 
-SELECT Nombre, Apellido FROM usuarios WHERE Id = 5 OR (Id = 6 AND Ocupacion = "estudiante") AND Apellido = "Perez";
+SELECT Nombre, Apellido FROM usuarios WHERE Id = 5 OR (Id = 6 AND Ocupacion = "estudiante") AND Apellido = "Viotti";
 
-SELECT * FROM usuarios WHERE Ocupacion = "estudiante" OR Ocupacion = "docente" AND Apellido = "Perez"Id = 5 OR Id = 6 ORDER BY Apellido LIMIT 2;
+SELECT * FROM usuarios WHERE Ocupacion = "estudiante" OR Ocupacion = "docente" AND Apellido = "Britos"Id = 5 OR Id = 6 ORDER BY Apellido LIMIT 2;
+
+SELECT COUNT(*) as "Usuarios desarrolladores" FROM usuarios WHERE Ocupacion = "desarrollador";
 
 
 
