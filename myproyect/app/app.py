435bb1e05e4d23a79aa0f3795ca8b14da941d818
marisa <marisaviotti@gmail.com>
2024-07-03 21:11:59 -0300

    app y templates

--- myproyect/app/app.py
+++ myproyect/app/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/index')
+def dataindex():
+    title = "Cartelera"
+    return render_template('index.html', Cartelera=title)
+
+@app.route('/contacto')
+def datacontacto():
+    title = "Contacto"
+    return render_template('contacto.html', Contacto=title)
+
+@app.route('/quienes-somos')
+def dataquienes():
+    title = "Quienes Somos"
+    return render_template('quienes-somos.html', QuienesSomos=title)
+
+@app.route('/criticas')
+def datacriticas():
+    title = "Criticas"
+    return render_template('criticas.html', Criticas=title)
+
+@app.route('/estrenos')
+def dataestrenos():
+    title = "Estrenos"
+    return render_template('estrenos.html', Estrenos=title)
+
